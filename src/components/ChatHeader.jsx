e5bef1fd9479198cd5afc183ecc2c56ed54004d4
yaxyobekb <pubgn9642@gmail.com>
2025-05-02 00:11:18 +0500

    Refactor ChatHeader component structure; update title and placeholder handling; adjust avatar rendering logic

--- src/components/ChatHeader.jsx
+++ src/components/ChatHeader.jsx
@@ -1,40 +1,42 @@
 import React from "react";
 
-const ChatHeader = ({
-  title = "",
-  avatar = "",
-  placeholder = "Xabar yozing...",
-}) => {
+const ChatHeader = ({ title = "", avatar = "", placeholder = "" }) => {
   return (
     <header className="bg-white p-4 border-b">
       <div className="flex items-center justify-between">
         {/* Profile */}
-        <button className="flex items-center gap-3">
+        <div className="flex items-center gap-3">
           {/* Avatar */}
-          <div className="flex items-center justify-center size-[46px] bg-gradient-to-tr from-blue-300 to-blue-600 rounded-full">
-            <svg
-              fill="none"
-              className="size-6"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill="white"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z"
-              />
-            </svg>
-          </div>
+          {false && (
+            <div className="flex items-center justify-center size-[46px] bg-gradient-to-tr from-blue-300 to-blue-600 rounded-full">
+              <svg
+                fill="none"
+                className="size-6"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill="white"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z"
+                />
+              </svg>
+            </div>
+          )}
 
           {/* Details */}
           <div className="text-start space-y-0.5">
             <h3 className="text-[17px] leading-6 font-medium">
-              Saqlangan xabarlar
+              {title || "Sarlavha mavjud emas"}
             </h3>
-            <p className="text-neutral-400 leading-5">Eslatma xabarlar</p>
+
+            {/* Placeholder */}
+            <p className="text-neutral-400 leading-5">
+              {placeholder || "Holat mavjud emas"}
+            </p>
           </div>
-        </button>
+        </div>
 
         {/* Actions button */}
         <button className="flex items-center justify-center size-12 rounded-full transition-colors duration-300 border border-transparent hover:bg-neutral-50 active:border-neutral-200">
