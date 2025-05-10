20b02e323fe7a7c4651661069a3aea200aa231ff
yaxyobekb <pubgn9642@gmail.com>
2025-05-10 11:16:21 +0500

    Refactor CSS structure: move styles from index.css to styles/index.css and update import paths

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { createRoot } from "react-dom/client";
 
 // CSS (styles)
-import "./index.css";
+import "./styles/index.css";
 
 // Components
 import App from "./App.jsx";
 
+// Redux
+import store from "./store/store.js";
+import { Provider } from "react-redux";
+
 // Render the project
-createRoot(document.getElementById("root")).render(<App />);
+const elRoot = document.getElementById("root");
+createRoot(elRoot).render(<Provider store={store} children={<App />} />);
