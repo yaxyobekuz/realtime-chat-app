cb53f935423ed171ce3177d644a74493098493af
yaxyobekb <pubgn9642@gmail.com>
2025-05-12 00:34:48 +0500

    Add skeleton loader for messages

--- src/components/ChatBody.jsx
+++ src/components/ChatBody.jsx
@@ -1,13 +1,23 @@
 import React, { useEffect, useRef } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
+
+// Redux (Store)
+import { useSelector } from "react-redux";
 
 // Components
 import TextMessageItem from "./TextMessageItem";
 import PhotoMessageItem from "./PhotoMessageItem";
 
-const ChatBody = ({ messages = [] }) => {
+const className = `max-h-[calc(100%-128px)] size-full overflow-y-auto hidden-scroll scroll-smooth scroll-pt-2 py-4`;
+
+const ChatBody = () => {
   const { hash } = useLocation();
   const chatContainerRef = useRef(null);
+  const { chatId: currentChatId } = useParams();
+  const chatId = Number(currentChatId) || false;
+  const { data, isLoading } = useSelector((state) => state.messages);
+
+  const messages = data[chatId]?.messages || [];
 
   // Scroll to the bottom when messages update
   useEffect(() => {
@@ -33,11 +43,26 @@ const ChatBody = ({ messages = [] }) => {
     return () => clearTimeout(timeoutId); // Cleanup
   }, [hash]);
 
+  // Loading
+  if (isLoading[chatId]) {
+    return (
+      <div ref={chatContainerRef} className={className}>
+        <ul className="flex flex-col gap-2 px-4 animate-pulse">
+          <li className="bg-neutral-100/60 max-w-32 w-full h-[42px] rounded-[21px] rounded-bl-lg" />
+          <li className="bg-neutral-100/60 max-w-44 w-full h-[42px] rounded-[21px] rounded-l-lg" />
+          <li className="bg-neutral-100/60 max-w-24 w-full h-[42px] rounded-[21px] rounded-tl-lg" />
+          <li className="bg-neutral-100/60 max-w-56 w-full h-[42px] rounded-[21px] ml-auto rounded-br-lg" />
+          <li className="bg-neutral-100/60 max-w-24 w-full h-[42px] rounded-[21px] ml-auto rounded-tr-lg" />
+          <li className="bg-neutral-100/60 size-80 rounded-[21px]" />
+          <li className="bg-neutral-100/60 max-w-64 w-full h-[42px] rounded-[21px] ml-auto" />
+        </ul>
+      </div>
+    );
+  }
+
+  // Messages
   return (
-    <div
-      ref={chatContainerRef}
-      className="max-h-[calc(100%-128px)] size-full overflow-y-auto hidden-scroll scroll-smooth scroll-pt-2 py-4"
-    >
+    <div ref={chatContainerRef} className={className}>
       <ul className="flex flex-col">
         {messages.map((msg, i) => {
           const isFirst = i === 0;
