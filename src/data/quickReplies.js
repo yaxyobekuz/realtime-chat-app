adea9bf6b6e28cedc32017dac90b9fc476957d68
yaxyobekb <pubgn9642@gmail.com>
2025-05-25 20:55:22 +0500

    Add quick replies data for user interactions

---
+++ src/data/quickReplies.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    command: "salom",
+    text: "Assalomu alaykum hurmatli foydalanuvchi.",
+  },
+  {
+    command: "kelishuv",
+    text: "Agar siz bizdan chipta sotib olmoqchi bo'lsangiz quyidagi shartlarga rozi bo'lishingiz kerak. Shartlar:\n\n1. Lorem, ipsum dolor sit amet consectetur adipisicing elit.\n2. Ullam accusantium omnis, maiores nobis architecto vero ipsum voluptatibus reiciendis nihil ad.",
+  },
+  {
+    command: "eslatma",
+    text: "⚠️ Eslatma!\n\nNarxlar Xabarda e'lon qilingan vaqtlardagina amal qiladi. Chipta yozilishi va pulingizga 100% kafolat beramiz. Reys qoldirilishi yoki bekor bo'lishiga bizning aloqamiz bo'lmaydi.\n\n🎒 Bagaj: 15 kg 👜 Qo'l yuki: 5 kg",
+  },
+];
