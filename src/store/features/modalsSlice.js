3fb04fddd0caaad3717f93a06aa7f4c76e14414d
yaxyobekb <pubgn9642@gmail.com>
2025-06-02 14:13:36 +0500

    Add modalsSlice for managing modal states in the application

---
+++ src/store/features/modalsSlice.js
@@ -0,0 +1,38 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialModalData = { isOpen: false, data: null };
+
+// Initial state
+const initialState = { payment: initialModalData };
+
+export const modalsSlice = createSlice({
+  initialState,
+  name: "modals",
+  reducers: {
+    openModal: (state, action) => {
+      const { name, data = null } = action.payload;
+      state[name] = { isOpen: true, data };
+    },
+
+    closeModal: (state, action) => {
+      const name = action.payload;
+      if (state[name]) {
+        state[name].data = null;
+        state[name].isOpen = false;
+      }
+    },
+
+    closeAllModals: (state) => {
+      for (const key in state) {
+        state[key].data = null;
+        state[key].isOpen = false;
+      }
+    },
+  },
+});
+
+// Export actions
+export const { closeAllModals, closeModal, openModal } = modalsSlice.actions;
+
+// Export reducer
+export default modalsSlice.reducer;
--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 
 // Features
 import chatsSlice from "./features/chatsSlice";
+import modalsSlice from "./features/modalsSlice";
 import messagesSlice from "./features/messagesSlice";
 
 export default configureStore({
   reducer: {
     chats: chatsSlice,
+    modals: modalsSlice,
     messages: messagesSlice,
   },
 });
