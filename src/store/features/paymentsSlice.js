b689652e8c05f8cc5b33d106bd47009300cdba39
yaxyobekb <pubgn9642@gmail.com>
2025-06-09 22:46:18 +0500

    Add payments slice and integrate into store configuration

---
+++ src/store/features/paymentsSlice.js
@@ -0,0 +1,55 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// Initial state
+const initialState = { data: [], hasError: false, isLoading: false };
+
+export const paymentsSlice = createSlice({
+  initialState,
+  name: "payments",
+  reducers: {
+    updatePaymentsFromStore: (state, action) => {
+      state.data = action.payload;
+    },
+
+    addNewPaymentToStore: (state, action) => {
+      state.data.push(action.payload);
+    },
+
+    removePaymentFromStore: (state, action) => {
+      state.data = state.data.filter(
+        (payment) => payment.id !== action.payload
+      );
+    },
+
+    updateSinglePaymentInStore: (state, { payload }) => {
+      const index = state.data.findIndex(
+        (payment) => payment.id === payload.id
+      );
+
+      if (index !== -1) {
+        state.data[index] = { ...state.data[index], ...payload };
+      }
+    },
+
+    setPaymentsLoading: (state, action) => {
+      state.isLoading = action.payload;
+    },
+
+    setPaymentsError: (state, action) => {
+      state.hasError = action.payload;
+    },
+  },
+});
+
+// Export actions
+export const {
+  setPaymentsError,
+  setPaymentsLoading,
+  addNewPaymentToStore,
+  removePaymentFromStore,
+  updatePaymentsFromStore,
+  updateSinglePaymentInStore,
+} = paymentsSlice.actions;
+
+// Export reducer
+export default paymentsSlice.reducer;
--- src/store/store.js
+++ src/store/store.js
@@ -4,11 +4,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import chatsSlice from "./features/chatsSlice";
 import modalsSlice from "./features/modalsSlice";
 import messagesSlice from "./features/messagesSlice";
+import paymentsSlice from "./features/paymentsSlice";
 
 export default configureStore({
   reducer: {
     chats: chatsSlice,
     modals: modalsSlice,
     messages: messagesSlice,
+    payments: paymentsSlice,
   },
 });
