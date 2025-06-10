de831a80193bdb79986d53c1301aad121dfab747
yaxyobekb <pubgn9642@gmail.com>
2025-06-10 21:53:01 +0500

    Add lodash dependency to package.json and package-lock.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -65,6 +65,7 @@ export default {
       },
 
       borderRadius: {
+        20: "20px",
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
