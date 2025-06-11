cc932710ea32829af4fc6dc22393b439318341e6
yaxyobekb <pubgn9642@gmail.com>
2025-06-11 22:53:14 +0500

    Add Table component for skeleton loading UI

---
+++ src/components/skeleton/Table.jsx
@@ -0,0 +1,14 @@
+const Table = () => {
+  return (
+    <div className="p-5 animate-pulse max-h-[calc(100%-64px)] overflow-y-auto">
+      <div className="overflow-hidden border rounded-20">
+        <div className="w-full h-14 bg-white border-b" />
+        {Array.from({ length: 20 }).map((_, index) => (
+          <div key={index} className="w-full h-12 even:bg-white/70"></div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Table;
