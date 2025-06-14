fac3056451af58da4691655bd18cefa0f3d12253
yaxyobekb <pubgn9642@gmail.com>
2025-06-14 12:01:34 +0500

    Add colors module with predefined color gradients

---
+++ src/data/colors.js
@@ -0,0 +1,14 @@
+const colors = {
+  0: "from-green-300 to-green-500",
+  1: "from-blue-300 to-blue-500",
+  2: "from-pink-300 to-pink-500",
+  3: "from-gold-300 to-gold-500",
+  4: "from-red-300 to-red-500",
+  5: "from-teal-300 to-teal-500",
+  6: "from-lime-300 to-lime-500",
+  7: "from-violet-300 to-violet-500",
+  8: "from-sky-300 to-sky-500",
+  9: "from-orange-300 to-orange-500",
+};
+
+export default colors;
