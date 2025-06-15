798a3db97781ee3c165151f1a37c45b0ee839ccf
Batman <pubgman.xy@gmail.com>
2025-06-15 09:28:58 +0500

    Integrate ticket statuses into TicketPageBody and TicketsList for improved status display

--- src/components/TicketPageBody.jsx
+++ src/components/TicketPageBody.jsx
@@ -7,18 +7,19 @@ import { formatDate } from "@/utils/helpers";
 // Toast (Notification)
 import { toast } from "@/notification/toast";
 
-// Components
-import TicketSkeleton from "./skeleton/Ticket";
+// Data
+import ticketStatuses from "@/data/ticketStatuses";
 
 // Hooks
 import useImageViewer from "@/hooks/useImageViewer";
 
-// Icons
+// Components
+import UserPhoto from "./UserPhoto";
+import TicketSkeleton from "./skeleton/Ticket";
 import TicketFileUploader from "./TicketFileUploader";
 
 // Services
 import ticketService from "@/api/services/ticketService";
-import UserPhoto from "./UserPhoto";
 
 const TicketPageBody = () => {
   const { ticketId } = useParams();
@@ -49,7 +50,8 @@ const TicketPageBody = () => {
   // Error
   if (!ticket) return "Xatolik";
 
-  const { file, passport, payment, user, _id: id } = ticket;
+  const { file, passport, payment, user, _id: id, status } = ticket;
+  const formattedStatus = ticketStatuses.find((s) => s.value === status);
 
   const handleViewImage = (type) => {
     if (type === "passport") {
@@ -59,24 +61,31 @@ const TicketPageBody = () => {
     }
   };
 
+  const handleFileUploaded = (fileData) => {
+    setTicket((prev) => {
+      let updatedStatus = prev.status;
+      if (prev.status === "new" && fileData) updatedStatus = "readyToSend";
+      if (prev.status === "readyToSend" && !fileData) updatedStatus = "new";
+      return { ...prev, status: updatedStatus, file: fileData };
+    });
+  };
+
   // Ticket content
   return (
     <div className="p-5 space-y-5">
       {/* Top */}
       <div className="grid grid-cols-3 gap-5">
         {/* File */}
-        <div className="w-full bg-white p-2.5 rounded-20 border">
+        <div className="w-full bg-white p-5 rounded-20 border">
           <TicketFileUploader
             file={file}
             ticketId={id}
-            onFileUploaded={(fileData) => {
-              setTicket((prev) => ({ ...prev, file: fileData }));
-            }}
+            onFileUploaded={handleFileUploaded}
           />
         </div>
 
         {/* Passport */}
-        <div className="w-full bg-white p-2.5 rounded-20 border">
+        <div className="w-full bg-white p-5 rounded-20 border">
           {/* Wrapper */}
           <div className="relative w-full mb-3">
             {/* Date */}
@@ -102,7 +111,7 @@ const TicketPageBody = () => {
         </div>
 
         {/* Payment */}
-        <div className="w-full bg-white p-2.5 rounded-20 border">
+        <div className="w-full bg-white p-5 rounded-20 border">
           {/* Wrapper */}
           <div className="relative w-full mb-3">
             {/* Date */}
@@ -134,7 +143,25 @@ const TicketPageBody = () => {
       </div>
 
       {/* Mid */}
-      <div className="grid grid-cols-2 gap-5">
+      <div className="grid grid-cols-3 gap-5">
+        {/* Status & Action button */}
+        <div className="flex items-center justify-between w-full bg-white p-5 rounded-20 border">
+          <div className="">
+            <h3 className="text-lg font-medium mb-3">Holat</h3>
+            <p className={formattedStatus?.color}>
+              {formattedStatus?.label || "Holat mavjud emas"}
+            </p>
+          </div>
+
+          <button
+            disabled={!file}
+            className="flex items-center justify-center h-12 px-5 bg-green-50 rounded-full text-green-600 transition-colors duration-200 hover:bg-green-100 disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-green-50"
+          >
+            Chiptani yuborish
+          </button>
+        </div>
+
+        {/* User info */}
         <div className="flex items-center gap-3.5 w-full bg-white p-5 rounded-20 border">
           {/* User photo */}
           <UserPhoto
