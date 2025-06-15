e8d63a4a94c858c7b0e8dbae51ad61fb00155805
Batman <pubgman.xy@gmail.com>
2025-06-15 09:28:38 +0500

    Add focus styles to BackLink component

--- src/components/BackLink.jsx
+++ src/components/BackLink.jsx
@@ -13,14 +13,14 @@ const BackLink = ({ href, label = "", title = "Ortga" }) => {
       aria-label={title}
       to={href ? href : -1}
       className={`${
-        label ? "pl-3.5 pr-6 hover:pr-3.5" : "w-12"
-      } flex items-center justify-center gap-3.5 h-12 rounded-full -mr-3.5 group transition-all duration-200 hover:mr-0 hover:bg-neutral-100`}
+        label ? "pl-3.5 pr-6 hover:pr-3.5 focus:pr-3.5" : "w-12"
+      } flex items-center justify-center gap-3.5 h-12 rounded-full -mr-3.5 group transition-all duration-200 hover:mr-0 hover:bg-neutral-100 focus:mr-0`}
     >
       <Icon
         size={22}
         alt="Ortga"
         src={arrowLeftIcon}
-        className="size-[22px] -ml-3.5 transition-[margin] duration-200 group-hover:ml-0.5"
+        className="size-[22px] -ml-3.5 transition-[margin] duration-200 group-hover:ml-0.5 group-focus:ml-0.5"
       />
       {label ? <span children={label} /> : null}
     </Link>
