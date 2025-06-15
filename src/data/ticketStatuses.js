7b0a26be07f3b76658e12b27194f0ce264cb3483
Batman <pubgman.xy@gmail.com>
2025-06-15 09:28:46 +0500

    Add ticketStatuses data

---
+++ src/data/ticketStatuses.js
@@ -0,0 +1,19 @@
+const ticketStatuses = [
+  {
+    label: "Yangi",
+    value: "new",
+    color: "text-neutral-500",
+  },
+  {
+    label: "Yuborishga tayyor",
+    value: "readyToSend",
+    color: "text-blue-500",
+  },
+  {
+    label: "Yuborilgan",
+    value: "sent",
+    color: "text-green-500",
+  },
+];
+
+export default ticketStatuses;
